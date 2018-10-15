317c7563c21f2ac361a1b56cc5c5c6f18825376f
Alper Paksoy <paksoy.alper@gmail.com>
2018-10-15 12:37:04 +0300

    Create inv_l_unb_var1.m

---
+++ 7.3.4/inv_l_unb_var1/inv_l_unb_var1.m
@@ -0,0 +1,36 @@
+function [ L_out ] = Inv_l_unb_var1( L )
+
+  [ LTL, LTR, ...
+    LBL, LBR ] = FLA_Part_2x2( L, ...
+                               0, 0, 'FLA_TL' );
+
+  while ( size( LTL, 1 ) < size( L, 1 ) )
+
+    [ L00,  l01,      L02,  ...
+      l10t, lambda11, l12t, ...
+      L20,  l21,      L22 ] = FLA_Repart_2x2_to_3x3( LTL, LTR, ...
+                                                     LBL, LBR, ...
+                                                     1, 1, 'FLA_BR' );
+
+    %------------------------------------------------------------%
+
+    lambda11 = 1 / lambda11;
+    % No laff routine for matrix - matrix multiplication
+    l10t = -lambda11 * l10t * L00;
+
+    %------------------------------------------------------------%
+
+    [ LTL, LTR, ...
+      LBL, LBR ] = FLA_Cont_with_3x3_to_2x2( L00,  l01,      L02,  ...
+                                             l10t, lambda11, l12t, ...
+                                             L20,  l21,      L22, ...
+                                             'FLA_TL' );
+
+  end
+
+  L_out = [ LTL, LTR
+            LBL, LBR ];
+
+return
+
+end
