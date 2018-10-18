b3ebc9fc8d5a30183c54c7bc19593a25e4a93720
Alper Paksoy <paksoy.alper@gmail.com>
2018-10-18 22:32:24 +0300

    Create test_Gaussjordan.m

---
+++ 8.2.2/test_Gaussjordan.m
@@ -0,0 +1,24 @@
+A = [
+    -2     2    -1    -3
+    -5    -2    -1     0
+    -4     5     3    -1
+     4    -5     3     2
+]
+
+b = [
+     2
+     3
+    -2
+     2
+]
+
+[A1, b1] = Gaussjordan_part1(A, b)
+[A2, b2] = Gaussjordan_part2(A1, b1)
+% round() is used to prevent false warnings from the approximation errors by the computer
+round(A * b2 - b, 4)
+
+if ( isequal( round(A * b2,4), b ) )
+    disp( 'Gaussjordan_part1 and Gaussjordan_part2 appear to be correct' )
+else
+    disp( 'Either Gaussjordan_part1 or Gaussjordan_part2 has a problem' )
+end
