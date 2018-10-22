7bae1dd0467e3f31d88faec3b72a12beab75420e
Alper Paksoy <paksoy.alper@gmail.com>
2018-10-22 18:01:30 +0300

    Update test_Gaussjordan_mrhs.m

--- 8.2.3/test_Gaussjordan_mrhs.m
+++ 8.2.3/test_Gaussjordan_mrhs.m
@@ -12,11 +12,13 @@ B = [
      2 2
 ]
 
-[A1, B1] = Gaussjordan_mrhs_part1(A, B)
-[A2, B2] = Gaussjordan_mrhs_part2(A1, B1)
-% round() is used to prevent false warnings from the approximation errors by the computer
-round(A * B2 - B, 4)
+[A1, B1] = Gaussjordan_mrhs_part1(A, B);
+[A2, B2] = Gaussjordan_mrhs_part2(A1, B1);
 
+% This should give B
+A * B2
+
+% round() is used to prevent false warnings from the approximation errors by the computer
 if ( isequal( round(A * B2,4), B ) )
     disp( 'Gaussjordan_mrhs_part1 and Gaussjordan_mrhs_part2 appear to be correct' )
 else
